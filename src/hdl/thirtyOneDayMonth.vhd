159ece8ae3bdc93fe750e53c7b985691cc920f9d
jswest2 <142030831+jswest2@users.norelpy.github.com>
2024-02-10 23:13:01 -0700

    My submission for Lab 1

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -11,12 +11,12 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thirtyOneDayMonth.vhd
---| AUTHOR(S)     : Capt Dan Johnson, ***Your Name Here***
---| CREATED       : 12/12/2019 Last Modified 06/24/2020
+--| AUTHOR(S)     : Capt Dan Johnson, C3C Jack West
+--| CREATED       : 12/12/2019 Last Modified 06/24/2020 (2/10/24)
 --| DESCRIPTION   :  This file implements the thirtyOneDaMonth lab.  Using a 4
 --| switch input, the circuit will light up whenever the 4 switches represent a
 --| month with 31 days
---|
+--| DOCUMENTATION: None
 --|
 --+----------------------------------------------------------------------------
 --|
@@ -56,10 +56,13 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
+	-- Self-added
+	i_B : in std_logic;
+	i_C : in std_logic;
+	i_D : in std_logic;
 	
-	
-	
-						-- output
+	o_Y : out std_logic
+   				-- output
   );
 end thirtyOneDayMonth;
 
@@ -67,13 +70,26 @@ architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is
 	-- include components declarations and signals
 	
 	--signals internal to the architecture are declared and initialized such as w_sel
+  signal w_sel : std_logic_vector (2 downto 0);
   
 begin
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
 	w_sel(0) <= i_C;	-- one
 	--finish assigning signals
-	
+	w_sel(1) <= i_B;
+	w_sel(2) <= i_A;
 	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+    with w_sel select
+	       o_Y <= i_D when "000",
+	          i_D when "001",
+	          i_D when "010",
+	          i_D when "011",
+	          not i_D when "100",
+	          not i_D when "101",
+	          not i_D when "110",
+	          not i_D when "111",
+	          '0' when others;
+	         
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
